2bb33c37f28d1e7c7ebe634644e007dd17d27b2e
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-21 13:17:35 -0500

    project router add working

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const server = express();
 
+const ProjectRouter = require('./API/projects/projectRouter');
 server.use(express.json());
 
+server.use('/api/projects', ProjectRouter)
 server.get('/', (req, res) => {
     res.send('server start test')
 })
