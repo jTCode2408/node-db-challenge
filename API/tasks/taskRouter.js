daa1a38e0dad7bcab01105880ed81df6495647b5
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-21 13:28:12 -0500

    moving task router to correct file

--- API/tasks/taskRouter.js
+++ API/tasks/taskRouter.js
@@ -1,20 +1,3 @@
 //add task
 //get tasks(with project name & description included)
 
-const db = require('../../data/db-config')
-
-module.exports = {
-    add,
-    find
-}
-
-function add(task) {
-    return db('tasks').insert(task)
-}
-
-
-function find() {
-    return db('tasks')
-        .join('projects', 'project_id', 'projects.id')
-    .select('task_description', 'notes', 'tasks.id', 'projects.id', 'project', 'description')
-}
